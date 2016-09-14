995b43060360529231a3a9ddd8a1d76753a95640
Edwin Rivera <waehake@gmail.com>
2016-09-13 22:52:59 -0400

    bootstrap site with lots of configuration

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,29 @@ var webserver = require('gulp-webserver');
 var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-cleancss');
 
-
 //webserver task
 gulp.task('webserver', function(){
   //the directory is going to be named app
   gulp.src('app')
-      .pipe(webserver({
-        livereload: true,
-        open: true,
-        port: '3000'
-      }));
+  .pipe(webserver({
+    livereload: true,
+    open: true,
+    port: '8080'
+  }));
 });
 
 //minify js files in the js directory
 gulp.task('scripts', function(){
   gulp.src('/app/js/*.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('app.js/app.min.js'));
+  .pipe(uglify())
+  .pipe(gulp.dest('app.js/app.min.js'));
 });
 
 //minify css files in the css directory
 gulp.task('clean-css', function(){
   gulp.src('/app/css/*.css')
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest('/app/css/style.css.min'));
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(gulp.dest('/app/css/style.css.min'));
 });
 
 //watch for changes in the css and js files
